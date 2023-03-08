2f1fd89977dc8dc81825570c4b68b408244b01d2
Goldnew1317 <newdermed84@gmail.com>
2023-03-08 19:32:50 -0400

    Tarea 6

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,33 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  nombre:string="";
+  preds:string="";
+  msg:string="";
+  img:string="";
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+Predecir(){
+  this.http.get<any>('https://api.agify.io/?name='+this.nombre+'')
+      .subscribe(res => {
+        this.preds = res.age;
+        if(parseFloat(this.preds) <= 18){
+          this.msg = 'Joven';
+          this.img = 'assets/icon/joven.png';
+        } else {
+          if(parseFloat(this.preds) <=50){
+            this.msg = 'Adulto';
+            this.img = 'assets/icon/adulto.png';
+          } else {
+            this.msg = 'Viejo';
+            this.img = 'assets/icon/viejo.png';
+          }
+        }
+      })
+
+}
 
 }
